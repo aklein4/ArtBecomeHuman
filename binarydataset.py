ed4b4bf29efa7a9c40e372d8ed215d50fedc843c
Adam Klein <adam3207@gmail.com>
2022-10-19 21:41:56 -0700

    basic training

--- binarydataset.py
+++ binarydataset.py
@@ -6,6 +6,7 @@ import os
 from PIL import Image
 
 IMAGE_SIZE = 256
+MAX_SET_SIZE = 5
 
 class BinaryDataset(torch.utils.data.Dataset):
     def __init__(self, real_path, ai_path):
@@ -17,12 +18,16 @@ class BinaryDataset(torch.utils.data.Dataset):
             img = resize(to_image(Image.open(real_path+f)))
             if img.shape[0] == 3:
                 self.real_imgs.append(img)
+            if len(self.real_imgs) >=  MAX_SET_SIZE:
+                break
 
         self.ai_imgs = []
         for f in os.listdir(ai_path):
             img = resize(to_image(Image.open(ai_path+f)))
             if img.shape[0] == 3:
                 self.ai_imgs.append(img)
+            if len(self.ai_imgs) >=  MAX_SET_SIZE:
+                break
 
     def __len__(self):
         return len(self.real_imgs)+len(self.ai_imgs)
--- train.py
+++ train.py
@@ -7,15 +7,18 @@ from binarydataset import BinaryDataset
 
 def main():
 
-    dataset = BinaryDataset("./small_ai_1/", "./small_ai_2/")
-    train_loader = torch.utils.data.DataLoader(dataset)
+    dataset = BinaryDataset("./non_AI_art/", "./_digital/")
+    train_loader = torch.utils.data.DataLoader(dataset, shuffle=True, )
 
     model = EFFICIENTNET_V2_CUSTOM()
     optimizer = model.configure_optimizers()
 
-    logger = pl.loggers.CSVLogger(save_dir='lightning_logs', flush_logs_every_n_steps=10000)
+    model.train()
 
-    trainer = pl.Trainer(logger=logger, log_every_n_steps=1)
+    logger = pl.loggers.CSVLogger(save_dir='lightning_logs', flush_logs_every_n_steps=10000)
+    checkpoint_callback = pl.callbacks.ModelCheckpoint(dirpath="checkpoints", save_top_k=5, monitor="train_loss")
+
+    trainer = pl.Trainer(logger=logger, log_every_n_steps=1, callbacks=[checkpoint_callback])
     trainer.fit(model, train_loader)
 
 if __name__ == '__main__':
